1fe75bb36f3dc3bcfb42191740a32a339aca4e3f
Ratthapit <pimpitcha_kanitpanya@cmu.ac.th>
2023-11-24 21:46:22 +0700

    CM

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,22 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.print("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        AirPurifier f = new AirPurifier("Pro", "K69","Pink");
+        System.out.print("Model : "+f.model+"\nSerial No. : "+f.serialNo+"\nColor : "+f.color+"\nDimension : "+f.dimension+"\nPrice : "+f.price+"\n");
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        System.out.println("Status : "+f.power+"\nFan Speed : "+f.fanspeed+"\n");
+        f.turnOn(3);
+        System.out.println("Status : "+f.power+"\nFan Speed : "+f.fanspeed+"\n");
+
+        System.out.print("\n----------------------------------\n");
+
+        AirPurifier ranA = new AirPurifier();
+        System.out.print("Model : "+ranA.model+"\nSerial No. : "+ranA.serialNo+"\nColor : "+ranA.color+"\nDimension : "+ranA.dimension+"\nPrice : "+ranA.price+"\n");
+        System.out.println("Status : "+f.power+"\nFan Speed : "+f.fanspeed+"\n");
+        ranA.turnOn(6);
+        System.out.println("Status : "+f.power+"\nFan Speed : "+f.fanspeed+"\n");
+        System.out.print("\n----------------------------------\n");
+
     }
 }
